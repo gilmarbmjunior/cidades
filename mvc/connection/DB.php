9bf402510f2349ddaf23d31f6dccf5f5317af571
Gilmar Bezerra de Menezes Júnior <gilmarjunior@outlook.com>
2023-08-26 10:48:22 -0300

    20230826-1026

--- mvc/connection/DB.php
+++ mvc/connection/DB.php
@@ -2,7 +2,7 @@
 
 class DB
 {
-    public static function conn(): ?PDO
+    public static function conn()
     {
         try {
             $conn = new PDO('mysql:host=108.181.92.73;port=3306;dbname=cidades;charset=utf8', 'inclus', 'inclus@2023');
