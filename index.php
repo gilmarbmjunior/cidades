5a7685ed5e1ee7d74df3dd8364b8a9f4e51873cf
Gilmar Bezerra de Menezes Júnior <gilmarjunior@outlook.com>
2023-08-26 10:48:22 -0300

    20230826-1050

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php
 
+require_once 'mvc/connection/DB.php';
+
+/**
+ * SHOW TABLES
+ *
+ * DESCRIBE regiao
+ * DESCRIBE estado
+ * DESCRIBE municipio
+ *
+ * SELECT * FROM regiao
+ * SELECT * FROM estado
+ * SELECT * FROM municipio
+ */
+
+$query = 'SELECT * FROM municipio';
+
+$dao = DB::conn();
+$stmt = $dao->prepare($query);
+$stmt->execute();
+
+$lista = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<pre>';
+print_r($lista);
+echo '</pre>';
